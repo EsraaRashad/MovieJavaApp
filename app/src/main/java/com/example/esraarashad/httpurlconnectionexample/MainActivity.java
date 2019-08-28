eb8f75090f064bf35163a767dc53fa6e40bb3637
EsraaRashad <resraa52@gmail.com>
2019-08-28 12:47:49 +0200

    first commit

---
+++ app/src/main/java/com/example/esraarashad/httpurlconnectionexample/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.esraarashad.httpurlconnectionexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}

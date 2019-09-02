4c79f0bd2c14417eda2eb0f6faecbba8b126be4d
EsraaRashad <esraa.rashad@ibtikar.net.sa>
2019-09-02 21:28:26 +0200

    starting to create the adapter class for grid recycler view

--- app/src/main/java/com/example/esraarashad/httpurlconnectionexample/DetailsAdapter.java
+++ app/src/main/java/com/example/esraarashad/httpurlconnectionexample/DetailsAdapter.java
@@ -1,4 +1,32 @@
 package com.example.esraarashad.httpurlconnectionexample;
 
-public class DetailsAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DetailsAdapter extends RecyclerView.Adapter<DetailsAdapter.DetailsViewHolder>{
+
+    @NonNull
+    @Override
+    public DetailsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DetailsViewHolder detailsViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class DetailsViewHolder extends RecyclerView.ViewHolder{
+
+        public DetailsViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }

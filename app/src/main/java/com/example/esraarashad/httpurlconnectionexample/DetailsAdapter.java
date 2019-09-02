b829d3ccfad7949f866895a21432b12db9fa129b
EsraaRashad <esraa.rashad@ibtikar.net.sa>
2019-09-02 22:04:10 +0200

    completing Details Adapter and Activity & creating the ImageDetailsActivity

--- app/src/main/java/com/example/esraarashad/httpurlconnectionexample/DetailsAdapter.java
+++ app/src/main/java/com/example/esraarashad/httpurlconnectionexample/DetailsAdapter.java
@@ -1,32 +1,53 @@
 package com.example.esraarashad.httpurlconnectionexample;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class DetailsAdapter extends RecyclerView.Adapter<DetailsAdapter.DetailsViewHolder>{
+    private Context mContext;
+    private int[] mPlaceList;
+//    ImageView imgDetail =null;
+
+    public DetailsAdapter(Context mContext , int[] mPlaceList) {
+        this.mContext = mContext;
+        this.mPlaceList = mPlaceList;
+    }
 
     @NonNull
     @Override
     public DetailsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recyclerview_details_layout,
+                viewGroup, false);
+        return new DetailsViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull DetailsViewHolder detailsViewHolder, int i) {
+        detailsViewHolder.imgDetail.setImageResource(mPlaceList[i]);
+        detailsViewHolder.imgDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mPlaceList.length;
     }
 
     public class DetailsViewHolder extends RecyclerView.ViewHolder{
-
+        ImageView imgDetail ;
         public DetailsViewHolder(@NonNull View itemView) {
             super(itemView);
+            imgDetail = itemView.findViewById(R.id.img_details);
         }
     }
 }

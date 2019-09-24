4ed7a73d7b7b0cac472a60810a20350fca74e0f0
Esraa Rashad <esraa.rashad@ibtikar.net.sa>
2019-09-24 12:16:59 +0200

    mocking objects

--- app/src/test/java/com/example/esraarashad/httpurlconnectionexample/homepackage/presenter/HomePresenterTest.java
+++ app/src/test/java/com/example/esraarashad/httpurlconnectionexample/homepackage/presenter/HomePresenterTest.java
@@ -1,12 +1,25 @@
 package com.example.esraarashad.httpurlconnectionexample.homepackage.presenter;
 
+import com.example.esraarashad.httpurlconnectionexample.homepackage.model.IHomeModel;
+import com.example.esraarashad.httpurlconnectionexample.homepackage.view.IHomeView;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import static org.junit.Assert.*;
 
 public class HomePresenterTest {
+    @Mock
+    IHomeModel model;
+    IHomeView view;
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     @Before
     public void setUp() throws Exception {

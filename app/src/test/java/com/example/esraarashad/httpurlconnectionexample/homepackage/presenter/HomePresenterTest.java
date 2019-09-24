237e4f6da0140cc0cf2b0c3ebbe761fffc5175b7
Esraa Rashad <esraa.rashad@ibtikar.net.sa>
2019-09-24 12:08:15 +0200

    creating test class for home presenter

---
+++ app/src/test/java/com/example/esraarashad/httpurlconnectionexample/homepackage/presenter/HomePresenterTest.java
@@ -0,0 +1,22 @@
+package com.example.esraarashad.httpurlconnectionexample.homepackage.presenter;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HomePresenterTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void asyncPopular() {
+    }
+}

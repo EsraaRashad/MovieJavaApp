c598ffea28bfca6681b451fea142fbc75121fc67
Esraa Rashad <esraa.rashad@ibtikar.net.sa>
2019-09-25 17:43:09 +0200

    unit test

--- app/src/test/java/com/example/esraarashad/httpurlconnectionexample/homepackage/presenter/HomePresenterTest.kt
+++ app/src/test/java/com/example/esraarashad/httpurlconnectionexample/homepackage/presenter/HomePresenterTest.kt
@@ -1,15 +1,31 @@
 package com.example.esraarashad.httpurlconnectionexample.homepackage.presenter
 
+import com.example.esraarashad.httpurlconnectionexample.homepackage.model.IHomeModel
+import com.example.esraarashad.httpurlconnectionexample.homepackage.model.PopularPeopleModel.PeopleResults
+import com.example.esraarashad.httpurlconnectionexample.homepackage.view.IHomeView
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.mockito.Mock
+import org.mockito.Mockito.*;
+import org.mockito.MockitoAnnotations
 
 class HomePresenterTest {
+    lateinit var presenter :HomePresenter
+    lateinit var list: ArrayList<PeopleResults>
+
+    @Mock
+    lateinit var iHomeView: IHomeView
+    @Mock
+    lateinit var iHomeModel: IHomeModel
 
     @Before
     fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        presenter = HomePresenter(iHomeView,iHomeModel)
+        list= ArrayList<PeopleResults>()
     }
 
     @After
@@ -18,5 +34,7 @@ class HomePresenterTest {
 
     @Test
     fun asyncPopular() {
+        presenter.asyncPopular()
+        verify(iHomeView).setRecyclerViewAndAdapter(list)
     }
 }
